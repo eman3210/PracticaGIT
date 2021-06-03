36a78eee862788735d0285144381f201507090a8
Emanuel Rivas <manu_1019@hotmail.com>
2021-06-03 16:08:49 -0300

    agrego get y sout en main

--- src/app/Alumno.java
+++ src/app/Alumno.java
@@ -3,4 +3,13 @@ package app;
 public class Alumno {
 
 	private String name;
+	
+	public Alumno()
+	{
+		name = "Ema";
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -7,6 +7,8 @@ public class Main {
 		System.out.println("hola mundo");
 
 		Alumno alu = new Alumno();
+		
+		System.out.println(alu.getName());
 	}
 
 }
